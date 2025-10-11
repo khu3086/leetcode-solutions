5aa89540f77187f27db225e56fe92ccdd7bcb6ae
Khushi <khushi.nigamwork@gmail.com>
2025-10-12 03:27:27 +0530

    Time: 2100 ms (20.74%) | Memory: 37.2 MB (75.82%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        vector<int> prefix;
+        prefix.push_back(0);
+        int n=nums.size();
+        for(int i=0;i<nums.size();i++){
+            prefix.push_back(prefix[i]+nums[i]);
+        }
+        int ans=0;
+        for(auto x: prefix){
+            cout<<x<<endl;
+        }
+        n=prefix.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(prefix[j]-prefix[i]==k)
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
