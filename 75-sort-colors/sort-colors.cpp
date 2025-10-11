ff588ed5e2f214de09fa83d32e4f11bc4914e959
Khushi <khushi.nigamwork@gmail.com>
2025-10-12 02:37:29 +0530

    Time: 427 ms (0.80%) | Memory: 11.6 MB (48.99%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void printarr(vector<int>& nums){
+        for(auto x: nums){
+            cout<<x<<" ";
+        }
+        cout<<endl;
+    }
+    void sortColors(vector<int>& nums) {
+        int low=0, high=nums.size()-1;
+        int n=nums.size();
+        if(n<=1) return;
+        int curr=0;
+        while(curr<=high){
+            if(nums[curr]==2){
+                swap(nums[curr], nums[high]);
+                cout<<"swapped"<<nums[curr]<<"&"<<nums[high]<<endl;
+                high--;
+            }
+            else if(nums[curr]==1){
+                curr++;
+                cout<<"kuch ni bc"<<endl;
+            }
+            else{
+                swap(nums[curr], nums[low]);
+                cout<<"swapped"<<nums[curr]<<"&"<<nums[low]<<endl;
+                low++;
+                curr++;
+            }
+            printarr(nums);
+        }
+    }
+};
