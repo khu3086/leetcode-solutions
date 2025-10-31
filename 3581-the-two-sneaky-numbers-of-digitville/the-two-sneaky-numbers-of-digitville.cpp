005defe69b9f947013ef0468c21e7e1a0b38fab2
Khushi <khushi.nigamwork@gmail.com>
2025-11-01 01:48:39 +0530

    Time: 35 ms (9.05%) | Memory: 25.1 MB (99.60%) - LeetSync

---
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            nums[i]++;
+        }
+        for(int i=0;i<n;i++){
+            int absIdx=abs(nums[i])-1;
+            nums[absIdx]*=-1;
+        }
+        vector<int> ans;
+        for(int i=0;i<n-2;i++){
+            cout<<nums[i]<<" ";
+            if(nums[i]>0) ans.push_back(i);
+        }
+        return ans;
+    }
+};
