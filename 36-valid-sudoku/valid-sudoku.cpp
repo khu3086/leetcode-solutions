d12b5509e6c1eaa70306eb97c8b5370dc96b0b44
Khushi <khushi.nigamwork@gmail.com>
2025-11-03 04:16:08 +0530

    Time: 12 ms (9.75%) | Memory: 25.8 MB (13.28%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -7,15 +7,16 @@ public:
         int n=board.size();
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(board[i][j]=='.') continue;
-                int val=board[i][j]-'0';
-                if(rows[i].find(board[i][j])!=rows[i].end()) return false;
-                if(cols[j].find(board[i][j])!=cols[j].end()) return false;
-                int numBox=((i/3)*3)+j/3;
-                if(boxes[numBox].find(board[i][j])!=boxes[numBox].end()) return false;
-                rows[i].insert(board[i][j]);
-                cols[j].insert(board[i][j]);
-                boxes[numBox].insert(board[i][j]);
+                char c=board[i][j];
+                if(c=='.') continue;
+                int ctoi=c-'0';
+                int boxi=(i/3)*3+j/3;
+                if(rows[i].find(ctoi)!=rows[i].end()) return false;
+                if(cols[j].find(ctoi)!=cols[j].end()) return false;
+                if(boxes[boxi].find(ctoi)!=boxes[boxi].end()) return false;
+                rows[i].insert(ctoi);
+                cols[j].insert(ctoi);
+                boxes[boxi].insert(ctoi);
             }
         }
         return true;
