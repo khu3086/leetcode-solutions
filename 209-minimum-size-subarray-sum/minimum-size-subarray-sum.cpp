f3468f7548b7da61fa06dfd79b2aa84ddc228a8c
Khushi <khushi.nigamwork@gmail.com>
2025-11-03 04:05:50 +0530

    Time: 0 ms (100.00%) | Memory: 32.2 MB (6.86%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -6,13 +6,14 @@ public:
         int minlen=INT_MAX;
         for(int right=0;right<nums.size();right++){
             currsum+=nums[right];
-            // shrink window while it's valid
-            while (currsum >= target) {
-                minlen = min(minlen, right - left + 1);
-                currsum -= nums[left];
+            while(currsum>=target){
+                minlen=min(minlen, right-left+1);
+                if(currsum-nums[left]<target) break;
+                currsum-=nums[left];
                 left++;
             }
         }
-        return minlen==INT_MAX?0:minlen;
+        if(minlen==INT_MAX) return 0;
+        return minlen;
     }
 };
