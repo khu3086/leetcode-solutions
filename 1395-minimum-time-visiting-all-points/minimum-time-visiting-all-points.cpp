5bbc1a7b23f725b23a9ccc03310d72e842068d61
Khushi <khushi.nigamwork@gmail.com>
2026-01-12 12:28:23 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (26.76%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
@@ -8,7 +8,6 @@ public:
         for(int i=0;i<n-1;i++){
             dx=abs(points[i][0]-points[i+1][0]);
             dy=abs(points[i][1]-points[i+1][1]);
-            int additive=min(dx, dy);
             int maxitive=max(dx, dy);
             ans+=maxitive;
         }
