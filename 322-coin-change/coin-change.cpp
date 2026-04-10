8a6c165b5c0730e562da9b7fd8af13e0c2d5b9e4
Khushi <khushi.nigamwork@gmail.com>
2026-04-11 02:53:04 +0530

    Time: 27 ms (58.71%) | Memory: 17.9 MB (81.88%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        //vector<vector<int>> dp(amount+1, vector<int>(amount+1, -1));
+        //sort(coins.begin(), coins.end());
+        vector<int> dp(amount+1, INT_MAX);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++){
+            for(int j=0;j<coins.size();j++){
+                if(i-coins[j]>=0&& dp[i - coins[j]] != INT_MAX){
+                    dp[i]=min(dp[i-coins[j]]+1, dp[i]);
+                }
+            }
+        }
+        if(dp[amount]==INT_MAX) return -1;
+        return dp[amount];
+    }
+};
