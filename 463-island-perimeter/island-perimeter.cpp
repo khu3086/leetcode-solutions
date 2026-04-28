3ed8f6b2f1482a16171d567c7e0351569d95294b
Khushi <khushi.nigamwork@gmail.com>
2026-04-28 15:06:50 +0530

    Time: 31 ms (7.82%) | Memory: 105 MB (18.77%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& grid, int& edge, vector<vector<bool>>& visited, int row, int col, int i, int j){
-        if(i<0||i>=row||j<0||j>=col||visited[i][j]) return;
-        if(grid[i][j]==0) return;
+    void perimeter(vector<vector<int>>& grid, vector<vector<bool>>& visited, int& peri, int row, int col, int i, int j){
+        if(i<0||i>=row||j<0||j>=col||grid[i][j]==0||visited[i][j]) return;
+        if(i==0||(i>0&&grid[i-1][j]==0)){
+            peri++;
+        }
+        if(i==row-1||(i<row-1&&grid[i+1][j]==0)){
+            peri++;
+        }
+        if(j==0||(j>0&&grid[i][j-1]==0)){
+            peri++;
+        }
+        if(j==col-1||(j<col-1&&grid[i][j+1]==0)){
+            peri++;
+        }
         visited[i][j]=true;
-        if(i>0&&grid[i-1][j]) //1 cell up island hai
-        dfs(grid, edge, visited, row, col, i-1, j);
-        else edge++;
-
-        if(j>0&&grid[i][j-1]) //1 cell right island hai
-        dfs(grid, edge, visited, row, col, i, j-1);
-        else edge++;
-
-        if(i<row-1&&grid[i+1][j]) //1 cell down island hai
-        dfs(grid, edge, visited, row, col, i+1, j);
-        else edge++;
-
-        if(j<col-1&&grid[i][j+1]) //1 cell left island hai
-        dfs(grid, edge, visited, row, col, i, j+1);
-        else edge++;
-
+        perimeter(grid, visited, peri, row, col, i+1, j);
+        perimeter(grid, visited, peri, row, col, i-1, j);
+        perimeter(grid, visited, peri, row, col, i, j+1);
+        perimeter(grid, visited, peri, row, col, i, j-1);
     }
     int islandPerimeter(vector<vector<int>>& grid) {
         int row=grid.size();
         int col=grid[0].size();
         vector<vector<bool>> visited(row, vector<bool>(col, false));
-        int edge=0;
-        int i=0, j=0;
-        for(i=0;i<row;i++){
-            for(j=0;j<col;j++){
-                if(grid[i][j]==1)dfs(grid, edge, visited, row, col, i, j);
+        int per=0;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(grid[i][j]==1){
+                    perimeter(grid, visited, per, row, col, i, j);
+                }
             }
         }
-        return edge;
+        return per;
     }
 };
