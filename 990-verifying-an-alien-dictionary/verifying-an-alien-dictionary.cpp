7da401823e5f59008ce803387695e2bf44958e3d
Khushi <khushi.nigamwork@gmail.com>
2026-04-28 15:13:53 +0530

    Time: 3 ms (13.92%) | Memory: 12.6 MB (26.96%) - LeetSync

--- 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.cpp
+++ 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     bool isAlienSorted(vector<string>& words, string order) {
-        unordered_map<char, char> mymap;
+        unordered_map<char, char> map;
         for(int i=0;i<26;i++){
-            mymap[order[i]]='a'+i;
+            map[order[i]]='a'+i;
         }
-
         for(int i=0;i<words.size();i++){
-            for(int j=0;j<words[i].size();j++){
-                words[i][j]=mymap[words[i][j]];
-            }
+            for(int j=0;j<words[i].size();j++)
+            words[i][j]=map[words[i][j]];
         }
         return is_sorted(words.begin(), words.end());
     }
