8b8a50bd1ea582a8b836146b22744bdd6ccef830
Khushi <khushi.nigamwork@gmail.com>
2026-04-28 17:24:04 +0530

    Time: 11 ms (21.60%) | Memory: 20 MB (38.23%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,10 +1,10 @@
 class Solution {
-private:
+public:
     bool dfs(vector<vector<int>>& adj, vector<bool>& path, vector<bool>& vis, int curr){
         vis[curr]=true;
         path[curr]=true;
-        for(int next: adj[curr]){
-            if(!vis[next]){
+        for(auto next: adj[curr]){
+            if(!vis[next]) {
                 if(dfs(adj, path, vis, next)) return true;
             }
             else if(path[next]) return true;
@@ -12,16 +12,15 @@ private:
         path[curr]=false;
         return false;
     }
-public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
-        for(auto pre: prerequisites) adj[pre[1]].push_back(pre[0]);
-        vector<bool> vis(numCourses, false);
-        vector<bool> path(numCourses, false);
-        for(int i=0;i<numCourses;i++){
-            if(!vis[i]){
-                if(dfs(adj, path, vis, i)) return false;
-            }
+    bool canFinish(int num, vector<vector<int>>& pre) {
+        vector<vector<int>> adj(num);
+        for(auto x: pre){
+            adj[x[0]].push_back(x[1]);
+        }
+        vector<bool> path(num, false);
+        vector<bool> vis(num, false);
+        for(int i=0;i<num;i++){
+            if(dfs(adj, path, vis, i)) return false;
         }
         return true;
     }
