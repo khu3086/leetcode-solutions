72be7c22569880f200f696b2e9a715b81f2a8077
Khushi <khushi.nigamwork@gmail.com>
2026-04-30 23:15:04 +0530

    Time: 69 ms (41.25%) | Memory: 70.2 MB (58.89%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.cpp
+++ 1558-course-schedule-iv/course-schedule-iv.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
-    vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& pre, vector<vector<int>>& queries) {
-        // floyd warshall
-        vector<vector<bool>> reach(n, vector<bool>(n, false));
-        for(auto x: pre){
-            reach[x[0]][x[1]]=true;
+    vector<bool> checkIfPrerequisite(int num, vector<vector<int>>& pre, vector<vector<int>>& queries) {
+        vector<vector<bool>> reach(num, vector<bool>(num, false));
+        vector<vector<int>> adj(num);
+        vector<int> indegree(num, 0);
+        queue<int> q;
+        for(int i=0;i<pre.size();i++){
+            int src=pre[i][0];
+            int dest=pre[i][1];
+            adj[src].push_back(dest);
+            indegree[dest]++;
+            reach[src][dest]=true;
         }
-        for(int k=0;k<n;k++){
-            for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
-                    if(reach[i][k]&&reach[k][j]) reach[i][j]=true;
+        for(int i=0;i<num;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        while(!q.empty()){
+            int top=q.front();
+            q.pop();
+            for(auto src: adj[top]){
+                indegree[src]--;
+                if(indegree[src]==0) q.push(src);
+                for(int i=0;i<num;i++){
+                    reach[i][src]=reach[i][top]||reach[i][src];
                 }
             }
         }
         vector<bool> ans;
         for(auto x: queries){
-            if(reach[x[0]][x[1]]) ans.push_back(true);
-            else ans.push_back(false);
+            ans.push_back(reach[x[0]][x[1]]);
         }
         return ans;
     }
