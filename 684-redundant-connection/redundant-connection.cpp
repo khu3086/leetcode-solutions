59179eff272c039d95ec2febe6769147cdb18e60
Khushi <khushi.nigamwork@gmail.com>
2026-04-30 22:57:52 +0530

    Time: 7 ms (17.49%) | Memory: 13.4 MB (36.22%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,43 +1,40 @@
 class Solution {
 public:
-    int find(int x, vector<int>& parent) {
-        if (parent[x] != x)
-            parent[x] = find(parent[x], parent);
+    vector<int> rank, parent;
+    int find(int x){
+        if(x!=parent[x]){
+            parent[x]=find(parent[x]);
+        }
         return parent[x];
     }
-
-    void unite(int a, int b, vector<int>& parent, vector<int>& rank) {
-        int pa = find(a, parent);
-        int pb = find(b, parent);
-
-        if (pa == pb) return;
-
-        if (rank[pa] < rank[pb]) {
-            parent[pa] = pb;
-        } else if (rank[pb] < rank[pa]) {
-            parent[pb] = pa;
-        } else {
-            parent[pb] = pa;
-            rank[pa]++;
+    bool unite(int x, int y){
+        int px=find(x);
+        int py=find(y);
+        if(px==py) return false;
+        if(rank[px]<rank[py]){
+            parent[px]=py;
         }
+        else if(rank[px]>rank[py]){
+            parent[py]=px;
+        }
+        else{
+            parent[py]=px;
+            rank[x]++;
+        }
+        return true;
     }
-
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int n = edges.size();
-        vector<int> parent(n + 1), rank(n + 1, 0);
-
-        for (int i = 1; i <= n; i++) parent[i] = i;
-
-        for (auto &e : edges) {
-            int u = e[0], v = e[1];
-
-            if (find(u, parent) == find(v, parent)) {
-                return e; // redundant
-            }
-
-            unite(u, v, parent, rank);
+        int n=edges.size();
+        parent.resize(n+1);
+        rank.resize(n+1);
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+            rank[i]=0;
         }
-
-        return {};
+        vector<int> ans;
+        for(auto edge:edges){
+            if(!unite(edge[0], edge[1])) ans=edge;
+        }
+        return ans;
     }
 };
