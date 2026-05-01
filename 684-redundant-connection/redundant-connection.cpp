b2930641791d62f37daa4900685238c65aa3acb9
Khushi <khushi.nigamwork@gmail.com>
2026-05-01 14:54:24 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (36.30%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,40 +1,43 @@
 class Solution {
 public:
-    vector<int> rank, parent;
+    vector<int> parent, rank;
     int find(int x){
-        if(x!=parent[x]){
-            parent[x]=find(parent[x]);
-        }
+        if(x==parent[x]) return x;
+        parent[x]=find(parent[x]);
         return parent[x];
     }
+
     bool unite(int x, int y){
         int px=find(x);
         int py=find(y);
         if(px==py) return false;
-        if(rank[px]<rank[py]){
-            parent[px]=py;
-        }
-        else if(rank[px]>rank[py]){
+        int rx=rank[px];
+        int ry=rank[py];
+        if(rx>ry){
             parent[py]=px;
+        }
+        else if(rx<ry){
+            parent[px]=py;
         }
         else{
             parent[py]=px;
-            rank[x]++;
+            rank[px]++;
         }
         return true;
     }
+
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n=edges.size();
         parent.resize(n+1);
         rank.resize(n+1);
-        for(int i=0;i<n;i++){
+        for(int i=0;i<=n;i++){
             parent[i]=i;
             rank[i]=0;
         }
-        vector<int> ans;
-        for(auto edge:edges){
-            if(!unite(edge[0], edge[1])) ans=edge;
+        for(auto edge: edges){
+            if(!unite(edge[0], edge[1])) return edge;
         }
-        return ans;
+        return {};
+
     }
 };
