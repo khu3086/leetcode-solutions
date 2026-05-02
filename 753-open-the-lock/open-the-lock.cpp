cc5cb8549d34811e8ec92f3d60264338b920b90f
Khushi <khushi.nigamwork@gmail.com>
2026-05-02 09:20:14 +0530

    Time: 139 ms (59.82%) | Memory: 34.9 MB (89.02%) - LeetSync

--- 753-open-the-lock/open-the-lock.cpp
+++ 753-open-the-lock/open-the-lock.cpp
@@ -2,88 +2,80 @@ class Solution {
 public:
     int openLock(vector<string>& deadends, string target) {
         unordered_set<string> dead;
-        queue<string> q;
         for(auto d: deadends) dead.insert(d);
-
         if(dead.find("0000")!=dead.end()) return -1;
-
-        q.push("0000");
+        if(dead.find(target)!=dead.end()) return -1;
         unordered_set<string> visited;
         visited.insert("0000");
-
-        int curr = 0;
-
+        queue<string> q;
+        q.push("0000");
+        int curr=0;
+        if(target=="0000") return curr;
         while(!q.empty()){
-            int k = q.size();
-
-            for(int i = 0; i < k; i++) {
-                string top = q.front();
+            int k=q.size();
+            for(int i=0;i<k;i++){
+                string top=q.front();
                 q.pop();
 
-                if(top == target) return curr;
-
-                string temp = top;
-
-                temp = top;
+                string temp=top;
                 temp[0]=(temp[0]-'0'+1)%10+'0';
-                if(dead.find(temp)==dead.end() && !visited.count(temp)) {
+                if(temp==target) return curr+1;
+                if(dead.find(temp)==dead.end()&&visited.find(temp)==visited.end()){
                     q.push(temp);
                     visited.insert(temp);
                 }
-
-                temp = top;
+                temp=top;
                 temp[0]=(temp[0]-'0'+9)%10+'0';
-                if(dead.find(temp)==dead.end() && !visited.count(temp)) {
+                if(temp==target) return curr+1;
+                if(dead.find(temp)==dead.end()&&visited.find(temp)==visited.end()){
                     q.push(temp);
                     visited.insert(temp);
                 }
-
-                temp = top;
-                temp[1]=(temp[1]-'0'+1)%10+'0';
-                if(dead.find(temp)==dead.end() && !visited.count(temp)) {
-                    q.push(temp);
-                    visited.insert(temp);
-                }
-
-                temp = top;
+                temp=top;
                 temp[1]=(temp[1]-'0'+9)%10+'0';
-                if(dead.find(temp)==dead.end() && !visited.count(temp)) {
+                if(temp==target) return curr+1;
+                if(dead.find(temp)==dead.end()&&visited.find(temp)==visited.end()){
                     q.push(temp);
                     visited.insert(temp);
                 }
-
-                temp = top;
-                temp[2]=(temp[2]-'0'+1)%10+'0';
-                if(dead.find(temp)==dead.end() && !visited.count(temp)) {
+                temp=top;
+                temp[1]=(temp[1]-'0'+1)%10+'0';
+                if(temp==target) return curr+1;
+                if(dead.find(temp)==dead.end()&&visited.find(temp)==visited.end()){
                     q.push(temp);
                     visited.insert(temp);
                 }
-
-                temp = top;
+                temp=top;
                 temp[2]=(temp[2]-'0'+9)%10+'0';
-                if(dead.find(temp)==dead.end() && !visited.count(temp)) {
+                if(temp==target) return curr+1;
+                if(dead.find(temp)==dead.end()&&visited.find(temp)==visited.end()){
                     q.push(temp);
                     visited.insert(temp);
                 }
-
-                temp = top;
-                temp[3]=(temp[3]-'0'+1)%10+'0';
-                if(dead.find(temp)==dead.end() && !visited.count(temp)) {
+                temp=top;
+                temp[2]=(temp[2]-'0'+1)%10+'0';
+                if(temp==target) return curr+1;
+                if(dead.find(temp)==dead.end()&&visited.find(temp)==visited.end()){
                     q.push(temp);
                     visited.insert(temp);
                 }
-
-                temp = top;
+                temp=top;
                 temp[3]=(temp[3]-'0'+9)%10+'0';
-                if(dead.find(temp)==dead.end() && !visited.count(temp)) {
+                if(temp==target) return curr+1;
+                if(dead.find(temp)==dead.end()&&visited.find(temp)==visited.end()){
+                    q.push(temp);
+                    visited.insert(temp);
+                }
+                temp=top;
+                temp[3]=(temp[3]-'0'+1)%10+'0';
+                if(temp==target) return curr+1;
+                if(dead.find(temp)==dead.end()&&visited.find(temp)==visited.end()){
                     q.push(temp);
                     visited.insert(temp);
                 }
             }
-
-            curr++; // ✅ moved outside for-loop
+            curr++;
         }
-
-        return -1; // ✅ correct final return
+        return -1;
     }
 };
