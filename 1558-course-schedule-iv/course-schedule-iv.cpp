e75475a240c65b0547fcbe5e4d021fc0fd5c5728
Khushi <khushi.nigamwork@gmail.com>
2026-05-02 09:40:15 +0530

    Time: 31 ms (76.60%) | Memory: 72.8 MB (40.99%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.cpp
+++ 1558-course-schedule-iv/course-schedule-iv.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
-    vector<bool> checkIfPrerequisite(int num, vector<vector<int>>& pre, vector<vector<int>>& queries) {
+    vector<bool> checkIfPrerequisite(int num, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
         vector<vector<bool>> reach(num, vector<bool>(num, false));
         vector<vector<int>> adj(num);
         vector<int> indegree(num, 0);
-        queue<int> q;
-        for(int i=0;i<pre.size();i++){
-            int src=pre[i][0];
-            int dest=pre[i][1];
-            adj[src].push_back(dest);
-            indegree[dest]++;
-            reach[src][dest]=true;
+        for(auto pre: prerequisites){
+            adj[pre[0]].push_back(pre[1]);
+            indegree[pre[1]]++;
+            reach[pre[0]][pre[1]]=true;
         }
+        queue<int> q;
         for(int i=0;i<num;i++){
             if(indegree[i]==0) q.push(i);
         }
         while(!q.empty()){
-            int top=q.front();
-            q.pop();
-            for(auto src: adj[top]){
-                indegree[src]--;
-                if(indegree[src]==0) q.push(src);
-                for(int i=0;i<num;i++){
-                    reach[i][src]=reach[i][top]||reach[i][src];
+            int top=q.front(); q.pop();
+            for(auto nei: adj[top]){
+                
+             for(int i = 0; i < num; i++){
+                    if(reach[i][top]){
+                        reach[i][nei] = true;
+                    }
                 }
+                indegree[nei]--;
+                if(indegree[nei]==0) q.push(nei);
             }
         }
         vector<bool> ans;
-        for(auto x: queries){
-            ans.push_back(reach[x[0]][x[1]]);
+        for(auto q: queries){
+            ans.push_back(reach[q[0]][q[1]]);
         }
         return ans;
     }
