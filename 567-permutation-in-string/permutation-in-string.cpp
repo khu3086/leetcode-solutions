f308fe3627033a062c036d876b78b08737935bdf
Khushi <khushi.nigamwork@gmail.com>
2026-06-16 16:24:35 +0530

    Time: 7 ms (40.46%) | Memory: 10.7 MB (24.88%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int k=s1.size(); //establishing window size
+        if (s2.size() < s1.size()) return false;
+        unordered_map<int, int> mp1;
+        unordered_map<int, int> mp2;
         int left=0;
-        unordered_map<char, int> mp1, mp2;
-        for(auto x: s1) mp1[x]++;
-        for(int right=0;right<s2.size();right++){
-            mp2[s2[right]]++;
-            while(right-left+1>k){
+        int window=s1.size();
+        int right=0;
+        for(auto c: s1) mp1[c]++;
+        for(int i=0;i<window;i++) mp2[s2[i]]++;
+        if(mp1==mp2) return true;
+        right=window;
+        while(right<s2.size()){
+            if(mp1!=mp2){
                 mp2[s2[left]]--;
                 if(mp2[s2[left]]==0) mp2.erase(s2[left]);
                 left++;
+                mp2[s2[right]]++;
+                right++;
+                if(mp1==mp2) return true;
             }
-            if(mp1==mp2) return true;
+            else return true;
         }
         return false;
     }
