be5ccc961723cedbafa9657ff3b02b5a62877911
Khushi <khushi.nigamwork@gmail.com>
2026-06-17 20:31:19 +0530

    Time: 0 ms (100.00%) | Memory: 35.9 MB (79.53%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        priority_queue<pair<int, int>> pq;
-        int n=arr.size();
-        for(int i=0;i<n;i++){
-            int dist=abs(arr[i]-x);
-            pq.push({dist, i});
-            if(pq.size()>k) pq.pop();
+        int left=0;
+        int right=arr.size()-k;
+        while(left<right){
+            int mid=(left+right)/2;
+            if(x-arr[mid]>arr[mid+k]-x){
+                left=mid+1;
+            }
+            else{
+                right=mid;
+            }
         }
-        vector<int> ans;
-        while(!pq.empty()){
-            ans.push_back(arr[pq.top().second]);
-            pq.pop();
-        }
-        sort(ans.begin(), ans.end());
-        return ans;
+        return vector<int>(arr.begin()+left, arr.begin()+left+k);
     }
 };
