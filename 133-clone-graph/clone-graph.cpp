a86620c38ed8272793ec3bca6cc3cfbe5205e140
Khushi <khushi.nigamwork@gmail.com>
2026-07-01 02:09:35 +0530

    Time: 3 ms (68.35%) | Memory: 11.9 MB (80.48%) - LeetSync

--- 133-clone-graph/clone-graph.cpp
+++ 133-clone-graph/clone-graph.cpp
@@ -1,45 +1,31 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    vector<Node*> neighbors;
-    Node() {
-        val = 0;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val) {
-        val = _val;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val, vector<Node*> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-};
-*/
-
 class Solution {
 public:
     Node* cloneGraph(Node* node) {
-         if (node == nullptr) return nullptr;
-        unordered_map<Node*, Node*> map;
+        if (node == nullptr)
+            return nullptr;
+
+        unordered_map<Node*, Node*> mp;
+
         queue<Node*> q;
         q.push(node);
-        map[node]=new Node(node->val);
-        while(!q.empty()){
-            Node* top=q.front();
+
+        mp[node] = new Node(node->val);
+
+        while (!q.empty()) {
+            Node* curr = q.front();
             q.pop();
-            for(auto x: top->neighbors){
-                if(map.find(x)==map.end()){
-                    map[x] = new Node(x->val);
-                    q.push(x);  // ✅ push to queue for BFS
+
+            for (Node* neigh : curr->neighbors) {
+
+                if (mp.find(neigh) == mp.end()) {
+                    mp[neigh] = new Node(neigh->val);
+                    q.push(neigh);
                 }
-                (map[top]->neighbors).push_back(map[x]);
+
+                mp[curr]->neighbors.push_back(mp[neigh]);
             }
-
         }
-        return map[node];
 
+        return mp[node];
     }
 };
