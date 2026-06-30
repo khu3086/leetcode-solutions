4ed2cbbf926299cf814a4f09da1326b841010e94
Khushi <khushi.nigamwork@gmail.com>
2026-07-01 02:21:04 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (90.65%) - LeetSync

--- 133-clone-graph/clone-graph.cpp
+++ 133-clone-graph/clone-graph.cpp
@@ -1,31 +1,45 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
 class Solution {
 public:
     Node* cloneGraph(Node* node) {
-        if (node == nullptr)
-            return nullptr;
-
-        unordered_map<Node*, Node*> mp;
-
+        if(node==nullptr) return nullptr;
         queue<Node*> q;
+        unordered_map<Node*, Node*> mp;
+        Node* newnode=new Node(node->val);
+        mp[node]=newnode;
         q.push(node);
-
-        mp[node] = new Node(node->val);
-
-        while (!q.empty()) {
-            Node* curr = q.front();
+        while(!q.empty()){
+            Node* top=q.front();
             q.pop();
-
-            for (Node* neigh : curr->neighbors) {
-
-                if (mp.find(neigh) == mp.end()) {
-                    mp[neigh] = new Node(neigh->val);
-                    q.push(neigh);
+            for(auto n: top->neighbors){
+                if(mp.find(n)==mp.end()){
+                    q.push(n);
+                     Node*  newneigh=new Node(n->val);
+                    mp[n]=newneigh;
                 }
-
-                mp[curr]->neighbors.push_back(mp[neigh]);
+                mp[top]->neighbors.push_back(mp[n]);
             }
         }
-
         return mp[node];
     }
 };
