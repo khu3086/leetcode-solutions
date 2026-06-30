190574ab144a08e80935972c6846268addc92de1
Khushi <khushi.nigamwork@gmail.com>
2026-06-30 17:39:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (93.00%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x, n);
+        long long exp=n;
+        if(exp<0){
+            x=1/x;
+            exp=-exp;
+        }
+        double ans=1;
+        while(exp){
+            if(exp&1) ans*=x;
+            x*=x;
+            exp=exp>>1;
+        }
+        return ans;
     }
 };
