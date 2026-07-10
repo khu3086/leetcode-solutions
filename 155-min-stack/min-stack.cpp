889cdcc7ab8fe4cac51d4b02f641b11072eb8797
Khushi <khushi.nigamwork@gmail.com>
2026-07-11 02:24:19 +0530

    Time: 77 ms (8.82%) | Memory: 151.3 MB (26.27%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,34 +1,41 @@
 class MinStack {
 public:
-    stack<int> minstack;
-    stack<int> regstack;
+    stack<int> stk;
+    stack<int> minstk;
     MinStack() {
         
     }
     
-    void push(int val) {
-        if(minstack.empty() || val <= minstack.top()) minstack.push(val);
-        regstack.push(val);
+    void push(int value) {
+        
+        if(!minstk.empty()&&minstk.top()>=value){
+            minstk.push(value);
+        }
+        else if(minstk.empty()) minstk.push(value);
+        stk.push(value);
     }
     
     void pop() {
-        if(minstack.top()==regstack.top()) minstack.pop();
-        regstack.pop();
+        if(!minstk.empty()&&!stk.empty()&&minstk.top()==stk.top()){
+            minstk.pop();
+        }
+        if(!stk.empty())stk.pop();
     }
     
     int top() {
-        return regstack.top();
+        return stk.top();
+
     }
     
     int getMin() {
-        return minstack.top();
+        return minstk.top();
     }
 };
 
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack* obj = new MinStack();
- * obj->push(val);
+ * obj->push(value);
  * obj->pop();
  * int param_3 = obj->top();
  * int param_4 = obj->getMin();
