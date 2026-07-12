1031e0e2429e580f5b8dae63a73361322267357a
Khushi <khushi.nigamwork@gmail.com>
2026-07-12 23:39:50 +0530

    Time: 87 ms (44.54%) | Memory: 47.9 MB (25.60%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<vector<pair<int, int>>> adj(n+1);
+        for(auto e: times){
+            adj[e[0]].push_back({e[1], e[2]});
+        }
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        pq.push({0, k});
         vector<int> dist(n+1, INT_MAX);
+        dist[0]=0;
         dist[k]=0;
-        for(int i=1;i<n;i++){
-            for(auto edge: times){
-               if (dist[edge[0]] != INT_MAX && dist[edge[0]] + edge[2] < dist[edge[1]]) {
-                    dist[edge[1]] = dist[edge[0]] + edge[2];
+        while(!pq.empty()){
+            auto [d, u]=pq.top();
+            pq.pop();
+            if(d>dist[u]) continue;
+            for(auto& [v, wt]: adj[u]){
+                if(d+wt<dist[v]){
+                    dist[v]=d+wt;
+                    pq.push({d+wt, v});
                 }
             }
         }
-        int res = 0;
-        for (int i = 1; i <= n; ++i) {
-            if (dist[i] == INT_MAX) return -1;
-            res = max(res, dist[i]);
+        int ans=0;
+        for(auto d: dist){
+            if(d==INT_MAX) return -1;
+            ans=max(ans, d);
         }
-        return res;
+        return ans;
+
     }
 };
