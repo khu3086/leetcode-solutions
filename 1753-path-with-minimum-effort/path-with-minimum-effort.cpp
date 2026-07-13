98e6a117929a43e942f3f559b950119abd19b0d0
Khushi <khushi.nigamwork@gmail.com>
2026-07-14 00:20:57 +0530

    Time: 175 ms (14.44%) | Memory: 53.9 MB (10.06%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,54 +1,37 @@
 class Solution {
 public:
-    int minimumEffortPath(vector<vector<int>>& heights) {
-        int m = heights.size();
-        int n = heights[0].size();
-
-        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
-        dist[0][0] = 0;
-
-        vector<vector<int>> dir = {{0,1}, {0,-1}, {1,0}, {-1,0}};
-
+    int minimumEffortPath(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<int>> dir={{0,1}, {0, -1}, {1, 0}, {-1, 0}};
         priority_queue<
-            pair<int, pair<int,int>>,
-            vector<pair<int, pair<int,int>>>,
-            greater<pair<int, pair<int,int>>>
-        > pq;
-
+    pair<int, pair<int,int>>,
+    vector<pair<int, pair<int,int>>>,
+    greater<pair<int, pair<int,int>>>
+> pq;
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
         pq.push({0, {0, 0}});
-
-        while (!pq.empty()) {
-            auto [effort, cell] = pq.top();
+        while(!pq.empty()){
+            auto top=pq.top();
             pq.pop();
-
-            int i = cell.first;
-            int j = cell.second;
-
-            // Skip stale entries
-            if (effort > dist[i][j]) continue;
-
-            // Reached destination
-            if (i == m - 1 && j == n - 1)
-                return effort;
-
-            for (auto &d : dir) {
-                int x = i + d[0];
-                int y = j + d[1];
-
-                if (x >= 0 && x < m && y >= 0 && y < n) {
-                    int newEffort = max(
-                        effort,
-                        abs(heights[i][j] - heights[x][y])
-                    );
-
-                    if (newEffort < dist[x][y]) {
-                        dist[x][y] = newEffort;
-                        pq.push({newEffort, {x, y}});
-                    }
+            int currDist=top.first;
+            int i=top.second.first;
+            int j=top.second.second;
+            if(visited[i][j]) continue;
+            visited[i][j]=true;
+            if(i==m-1&&j==n-1) return currDist;
+            for(auto d: dir){
+                int x=i+d[0]; int y=j+d[1];
+                if(x>=0&&x<m&&y>=0&&y<n){
+                    if(visited[x][y]) continue;
+                    int nextDist=abs(grid[i][j]-grid[x][y]);
+                    if(nextDist>dist[x][y]) continue;
+                    dist[x][y]=max(currDist, nextDist);
+                    pq.push({dist[x][y], {x, y}});
                 }
             }
         }
-
-        return 0;
+        return dist[m-1][n-1];
     }
 };
