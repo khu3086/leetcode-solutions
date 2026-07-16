ce8c81f05778d46df9fa8954618c732ed6bda1a3
Khushi <khushi.nigamwork@gmail.com>
2026-07-16 23:29:02 +0530

    Time: 4 ms (29.32%) | Memory: 9.2 MB (50.07%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        vector<unsigned long long> dp(target + 1, 0);
+        vector<unsigned int> dp(target+1, 0);
         dp[0]=1;
         for(int i=1;i<=target;i++){
-            for(auto x: nums){
-                if(i>=x){
-                    dp[i]+=dp[i-x];
-                }
+            
+            for(auto num: nums){
+                if(i-num>=0&&dp[i-num]!=0){
+                    dp[i]+=dp[i-num];
+                } 
             }
         }
         return dp[target];
