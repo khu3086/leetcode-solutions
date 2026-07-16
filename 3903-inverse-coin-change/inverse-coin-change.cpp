d56d852154ea28a9d7b1f4ed3080425f3b6ca2e5
Khushi <khushi.nigamwork@gmail.com>
2026-07-16 06:50:45 +0530

    Time: 0 ms (100.00%) | Memory: 26.8 MB (83.07%) - LeetSync

--- 3903-inverse-coin-change/inverse-coin-change.cpp
+++ 3903-inverse-coin-change/inverse-coin-change.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
     vector<int> findCoins(vector<int>& numWays) {
-        vector<int> coins;
-        int maxCoin=numWays.size();
-        int minCoin=1;
-        int n=maxCoin;
+        int n=numWays.size();
         vector<int> dp(n+1, 0);
+        dp[0]=1;
+        vector<int> coins;
         for(int i=1;i<=n;i++){
-            if(numWays[i-1]>=0){
-                //check 0 till i-1
-                //i==4
-                vector<int> miniDp(i+1, 0);
-                miniDp[0]=1;
-                for(auto x: coins){
-                    for(int j=x;j<=i;j++){
-                        if(j>=x&&miniDp[j-x]!=0){
-                            miniDp[j]+=miniDp[j-x];
-                        }
-                    }
-                }
-                if (miniDp[i] == numWays[i-1]){}
-                else if(numWays[i-1]-miniDp[i]==1){
-                    dp[i]=1;
-                    coins.push_back(i);
-                }
-                else return {};
-            }
+            if (dp[i] == numWays[i-1]) {
+    // nothing
+}
+else if (dp[i] + 1 == numWays[i-1]) {
+    coins.push_back(i);
+
+    for (int j = i; j <= n; j++) {
+        dp[j] += dp[j-i];
+    }
+}
+else {
+    return {};
+}
         }
         return coins;
     }
