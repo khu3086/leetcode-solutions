fb57dafc30d72d824063ac2006dae19001f2e3c8
Khushi <khushi.nigamwork@gmail.com>
2026-07-20 05:56:36 -0700

    Time: 94 ms (25.53%) | Memory: 48.1 MB (9.41%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -2,31 +2,39 @@ class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<vector<pair<int, int>>> adj(n+1);
-        for(auto e: times){
-            adj[e[0]].push_back({e[1], e[2]});
-        }
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
         pq.push({0, k});
-        vector<int> dist(n+1, INT_MAX);
-        dist[0]=0;
-        dist[k]=0;
+        for(auto t: times){
+            int u=t[0];
+            int v=t[1];
+            int dist=t[2];
+            adj[u].push_back({v, dist});
+        }
+        vector<int> distance(n+1, INT_MAX);
+        distance[k]=0;
+        distance[0]=0;
+        int sum=0;
         while(!pq.empty()){
-            auto [d, u]=pq.top();
+            auto top=pq.top();
             pq.pop();
-            if(d>dist[u]) continue;
-            for(auto& [v, wt]: adj[u]){
-                if(d+wt<dist[v]){
-                    dist[v]=d+wt;
-                    pq.push({d+wt, v});
+            int node=top.second;
+            int dist=top.first;
+            if(dist > distance[node]) continue;
+            for(auto nei: adj[node]){
+                int neinode=nei.first;
+                int neidist=nei.second;
+                
+                if(neidist+dist<distance[neinode]){
+                    distance[neinode]=neidist+dist;
+                    pq.push({distance[neinode], neinode});
                 }
             }
         }
         int ans=0;
-        for(auto d: dist){
-            if(d==INT_MAX) return -1;
-            ans=max(ans, d);
+        for(int i=0;i<=n;i++){
+            if(distance[i]==INT_MAX) return -1;
+            ans=max(ans, distance[i]);
         }
         return ans;
-
     }
 };
