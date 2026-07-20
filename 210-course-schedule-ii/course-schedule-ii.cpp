e53c455df0ef7920f4e043fe145d216679b7297f
Khushi <khushi.nigamwork@gmail.com>
2026-07-19 20:38:59 -0700

    Time: 8 ms (22.16%) | Memory: 18.9 MB (34.13%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -2,28 +2,26 @@ class Solution {
 public:
     vector<int> findOrder(int n, vector<vector<int>>& prereq) {
         vector<vector<int>> adj(n);
-        vector<int> indegree(n, 0);
+        vector<int> indeg(n, 0);
+        vector<int> ans;
         queue<int> q;
         for(auto p: prereq){
             adj[p[1]].push_back(p[0]);
-            indegree[p[0]]++;
+            indeg[p[0]]++;
         }
-        vector<int> ans;
-        vector<bool> vis(n, false);
         for(int i=0;i<n;i++){
-            if(indegree[i]==0) {q.push(i); vis[i]=true;}
+            if(indeg[i]==0) q.push(i);
         }
         while(!q.empty()){
             int top=q.front();
             q.pop();
             ans.push_back(top);
-            for(auto nxt: adj[top]){
-                indegree[nxt]--;
-                if(indegree[nxt]==0&&!vis[nxt]) q.push(nxt);
+            for(auto nei: adj[top]){
+                indeg[nei]--;
+                if(indeg[nei]==0) q.push(nei);
             }
         }
-        //reverse(ans.begin(), ans.end());
-        if(ans.size()==n) return ans;
-        return {};
+        if(ans.size()!=n) return {};
+        return ans;
     }
 };
