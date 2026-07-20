1382385bdd0a1e946e10ceb5dc8beffb3368ed1d
Khushi <khushi.nigamwork@gmail.com>
2026-07-19 19:00:08 -0700

    Time: 7 ms (81.24%) | Memory: 18.4 MB (74.36%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.cpp
+++ 1558-course-schedule-iv/course-schedule-iv.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    vector<bool> checkIfPrerequisite(int num, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
-        vector<vector<bool>> reach(num, vector<bool>(num, false));
-        vector<vector<int>> adj(num);
-        vector<int> indegree(num, 0);
-        for(auto pre: prerequisites){
-            adj[pre[0]].push_back(pre[1]);
-            indegree[pre[1]]++;
-            reach[pre[0]][pre[1]]=true;
+    vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& prereq, vector<vector<int>>& queries) {
+        vector<vector<int>> adj(n);
+        vector<vector<bool>> table(n, vector<bool>(n, false));
+        vector<int> indegree(n, 0);
+        for(auto p: prereq){
+            adj[p[1]].push_back(p[0]);
+            indegree[p[0]]++;
+            table[p[1]][p[0]]=true;
         }
         queue<int> q;
-        for(int i=0;i<num;i++){
-            if(indegree[i]==0) q.push(i);
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0) q.push(i); 
         }
         while(!q.empty()){
             int top=q.front(); q.pop();
+            //if curr node is prereq of something, it will be prereq of their nei
             for(auto nei: adj[top]){
-                
-             for(int i = 0; i < num; i++){
-                    if(reach[i][top]){
-                        reach[i][nei] = true;
-                    }
+                for(int i=0;i<n;i++){
+                    if(table[i][top]) table[i][nei]=true;
                 }
                 indegree[nei]--;
                 if(indegree[nei]==0) q.push(nei);
@@ -28,7 +26,7 @@ public:
         }
         vector<bool> ans;
         for(auto q: queries){
-            ans.push_back(reach[q[0]][q[1]]);
+            ans.push_back(table[q[1]][q[0]]);
         }
         return ans;
     }
