75f288465c84c7ffd57102e570c3cbb44999291e
Khushi <khushi.nigamwork@gmail.com>
2026-07-20 04:56:07 -0700

    Time: 55 ms (20.33%) | Memory: 46.4 MB (17.54%) - LeetSync

--- 721-accounts-merge/accounts-merge.cpp
+++ 721-accounts-merge/accounts-merge.cpp
@@ -12,43 +12,40 @@ public:
         parent[py]=px;
     }
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        unordered_map<string, vector<int>> mp;
         int n=accounts.size();
         parent.resize(n);
         for(int i=0;i<n;i++) parent[i]=i;
-        unordered_map<string, vector<int>> mp;
-        for(int j=0;j<n;j++){
-            vector<string> acc=accounts[j];
-            for(int i=1;i<acc.size();i++)
-            mp[acc[i]].push_back(j);
+        for(int i=0;i<n;i++){
+            for(int j=1;j<accounts[i].size();j++){
+                mp[accounts[i][j]].push_back(i);
+            }
         }
         for(auto m: mp){
-            vector<int> nodes=m.second;
-            for(int i=0;i<nodes.size()-1;i++){
-                unite(nodes[i], nodes[i+1]);
+            vector<int> temp=m.second;
+            int sz=temp.size();
+            for(int i=0;i<sz;i++){
+                for(int j=i+1;j<sz;j++) unite(temp[i], temp[j]);
             }
         }
         unordered_map<int, vector<int>> grp;
-        for(auto m: mp){
-            vector<int> nodes=m.second;
-            for(int i=0;i<nodes.size();i++){
-                grp[find(nodes[i])].push_back(nodes[i]);
-            }
+        for(int i=0;i<n;i++){
+            grp[find(i)].push_back(i);
         }
         vector<vector<string>> ans;
-        for(auto g: grp){
-            //ref node->g.first
-            int ref=g.first;
-            int num=g.second.size();
-            vector<int> trav=g.second;
-            set<string> st;
-            for(int i=0;i<num;i++){
-                for(int j=1;j<accounts[trav[i]].size();j++){
-                    st.insert(accounts[trav[i]][j]);
+        for(auto m: grp){
+            vector<string> temp;
+            set<string> temp2;
+            temp.push_back(accounts[m.first][0]);
+            int sz=m.second.size();
+            vector<int> arr=m.second;
+            for(int i=0;i<sz;i++){
+                //curr idx=arr[i]
+                for(int j=1;j<accounts[arr[i]].size();j++){
+                    temp2.insert(accounts[arr[i]][j]);
                 }
             }
-            vector<string> temp;
-            temp.push_back(accounts[ref][0]);
-            for(auto str: st) temp.push_back(str);
+            for(auto s: temp2) temp.push_back(s);
             ans.push_back(temp);
         }
         return ans;
