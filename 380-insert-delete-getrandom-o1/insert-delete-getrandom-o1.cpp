86dc769ef7c89d54899f5337eed24b7731ca54ac
Khushi <khushi.nigamwork@gmail.com>
2026-07-21 09:40:56 -0700

    Time: 40 ms (74.01%) | Memory: 113.2 MB (91.23%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
@@ -1,38 +1,39 @@
 class RandomizedSet {
 public:
-    vector<int> nums;
     unordered_map<int, int> mp;
+    vector<int> num;
     RandomizedSet() {
+        
     }
     
     bool insert(int val) {
         if(mp.find(val)==mp.end()){
-            mp[val]=nums.size();
-            nums.push_back(val);
+            num.push_back(val);
+            int n=num.size();
+            mp[val]=n-1;
             return true;
         }
-        return false;
+        else return false;
     }
     
     bool remove(int val) {
         if(mp.find(val)==mp.end()){
             return false;
         }
-        //swap last and required ele
-        int n=nums.size();
-        int num=nums[mp[val]];
-        int idx=mp[val];
-        int swapped=nums[n-1];
-        swap(nums[mp[val]], nums[n-1]);
-        nums.pop_back();
-        mp.erase(num);
-        if(num != swapped)
-        mp[swapped] = idx;
-        return true;
+        else{
+            int i=mp[val];
+            int n=num.size();
+            int temp=num[n-1];
+            swap(num[i], num[n-1]);
+            num.pop_back();
+            mp[temp]=i;
+            mp.erase(val);
+            return true;
+        }
     }
     
     int getRandom() {
-        return nums[rand()%nums.size()];
+        return num[rand()%(num.size())];
     }
 };
 
