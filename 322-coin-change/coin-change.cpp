4a812ea4e3102b4135c4cbb007765b3957cd0e37
Khushi <khushi.nigamwork@gmail.com>
2026-07-21 02:36:58 -0700

    Time: 24 ms (66.44%) | Memory: 17.8 MB (84.46%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -4,9 +4,9 @@ public:
         vector<int> dp(amount+1, INT_MAX);
         dp[0]=0;
         for(int i=1;i<=amount;i++){
-            for(int j=0;j<coins.size();j++){
-                if(i-coins[j]>=0&& dp[i - coins[j]] != INT_MAX){
-                    dp[i]=min(dp[i-coins[j]]+1, dp[i]);
+            for(auto coin: coins){
+                if(i-coin>=0&&dp[i-coin]!=INT_MAX){
+                    dp[i]=min(dp[i], dp[i-coin]+1);
                 }
             }
         }
