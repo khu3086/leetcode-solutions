e6a5f6dbcc54ed3c5020a4eee4f2e5102adb8ae9
Khushi <khushi.nigamwork@gmail.com>
2026-07-22 01:11:12 -0700

    Time: 177 ms (13.53%) | Memory: 53.6 MB (9.96%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
+        //pair<int, pair<int, int>> //dist, x, y
         int m=heights.size();
         int n=heights[0].size();
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
         vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
         dist[0][0]=0;
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
         pq.push({0, {0, 0}});
-        vector<vector<int>> dir={{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        vector<vector<int>> dir={{0,1}, {1,0}, {-1, 0}, {0, -1}};
         while(!pq.empty()){
-            auto top=pq.top(); pq.pop();
-            int distance=top.first;
+            auto top=pq.top();
+            pq.pop();
+            int currdist=top.first;
             int i=top.second.first;
             int j=top.second.second;
-            if(i==m-1&&j==n-1) return distance;
-            for(auto d: dir){
-                int x=d[0]+i;
-                int y=d[1]+j;
+            if (currdist > dist[i][j]) continue;
+            if(i == m-1 && j == n-1)
+        return currdist;
+            for(auto d:dir){
+                int x=i+d[0]; int y=j+d[1];
                 if(x>=0&&x<m&&y>=0&&y<n){
-                    int newEffort = max(distance, abs(heights[x][y] - heights[i][j]));
-                    if(newEffort>=dist[x][y]) continue;
-                    dist[x][y]=newEffort;
+                    int newdist=max(abs(heights[i][j]-heights[x][y]), currdist);
+                    if(newdist>=dist[x][y]) continue;
+                    dist[x][y]=(newdist);
                     pq.push({dist[x][y], {x, y}});
                 }
             }
