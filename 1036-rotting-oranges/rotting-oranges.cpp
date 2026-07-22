8191c880cca7fef82f9ea54e4f044372ed048872
Khushi <khushi.nigamwork@gmail.com>
2026-07-22 00:51:01 -0700

    Time: 3 ms (26.34%) | Memory: 18 MB (5.26%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,55 +1,41 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        queue<pair<int, int>> q;
         int fresh=0;
-        int row=grid.size();
-        int col=grid[0].size();
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(grid[i][j]==2) q.push({i, j});
+        int m=grid.size();
+        int n=grid[0].size();
+        queue<pair<int, int>> q;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]==1) fresh++;
+                else if(grid[i][j]==2) q.push({i, j});
             }
         }
         if(fresh==0) return 0;
-        if(q.empty()) return -1;
-        int curr=0;
+        vector<vector<int>> dir={{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        int count=0;
         while(!q.empty()){
-            bool spread=false;
-            int k=q.size();
-            for(int i=0;i<k;i++)
-            {auto top=q.front();
-            q.pop();
-            
-            int x=top.first;
-            int y=top.second;
-            if(x>0&&grid[x-1][y]==1){
-                spread=true;
-                grid[x-1][y]=2;
-                q.push({x-1, y});
-                fresh--;
+            int sz=q.size();
+            count++;
+            while(sz){
+                auto top=q.front(); q.pop();
+                int i=top.first;
+                int j=top.second;
+                for(auto d: dir){
+                    int x=i+d[0]; int y=j+d[1];
+                    if(x>=0&&x<m&&y>=0&&y<n){
+                        if(grid[x][y]==1){
+                            fresh--;
+                            grid[x][y]=2;
+                            q.push({x, y});
+                        }
+                    }
+                }
+                sz--;
             }
-            if(y>0&&grid[x][y-1]==1){
-                spread=true;
-                grid[x][y-1]=2;
-                q.push({x, y-1});
-                fresh--;
-            }
-            if(x<row-1&&grid[x+1][y]==1){
-                spread=true;
-                grid[x+1][y]=2;
-                q.push({x+1, y});
-                fresh--;
-            }
-            if(y<col-1&&grid[x][y+1]==1){
-                spread=true;
-                grid[x][y+1]=2;
-                q.push({x, y+1});
-                fresh--;
-            }}
-            if(spread) curr++;
         }
-        if(fresh) return -1;
-        return curr;
+        if(fresh==0)
+        return count-1;
+        return -1;
     }
 };
