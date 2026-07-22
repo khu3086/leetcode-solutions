c6e7281c5eca32ad89e035ef6e145ad82ddc7579
Khushi <khushi.nigamwork@gmail.com>
2026-07-22 16:16:08 -0700

    Time: 4 ms (59.36%) | Memory: 14.7 MB (80.90%) - LeetSync

--- 974-reorder-data-in-log-files/reorder-data-in-log-files.cpp
+++ 974-reorder-data-in-log-files/reorder-data-in-log-files.cpp
@@ -3,31 +3,21 @@ public:
     vector<string> reorderLogFiles(vector<string>& logs) {
         vector<pair<string, string>> letter;
         vector<string> digit;
-
-        for (auto &log : logs) {
-            int pos = log.find(' ');
-
-            if (isalpha(log[pos + 1])) {
-                string id = log.substr(0, pos);
-                string content = log.substr(pos + 1);
-                letter.push_back({content, id});
-            } else {
+        for(auto log: logs){
+            int pos=log.find(' ');
+            if(log[pos+1]<='z'&&log[pos+1]>='a'){
+                letter.push_back({log.substr(pos+1), log.substr(0, pos)});
+            }
+            else{
                 digit.push_back(log);
             }
         }
-
         sort(letter.begin(), letter.end());
-
         vector<string> ans;
-
-        for (auto &p : letter) {
-            ans.push_back(p.second + " " + p.first);
+        for(auto l: letter){
+            ans.push_back(l.second+" "+l.first);
         }
-
-        for (auto &d : digit) {
-            ans.push_back(d);
-        }
-
+        for(auto d: digit) ans.push_back(d);
         return ans;
     }
 };
