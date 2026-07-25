71f1be80a1d645a8e98ac33dfa5db6910f898c92
Khushi <khushi.nigamwork@gmail.com>
2026-07-25 06:58:37 -0700

    Time: 188 ms (7.07%) | Memory: 71.2 MB (5.01%) - LeetSync

--- 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.cpp
+++ 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.cpp
@@ -14,7 +14,7 @@ public:
         }
         if(count==0) return -1;
         else if(count==m*n) return -1;
-        int maxval=INT_MIN;
+        int maxval=1;
         vector<vector<int>> dir={{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
         while(!q.empty()){
             int sz=q.size();
@@ -26,7 +26,7 @@ public:
                     int x=i+d[0]; int y=j+d[1];
                     if(x>=0&&x<m&&y>=0&&y<n){
                         if(vis[x][y]==0){
-                            vis[x][y]=vis[i][j]+1;
+                            vis[x][y]=abs(x-i)+abs(y-j)+vis[i][j];
                             maxval=max(maxval, vis[x][y]);
                             q.push({x, y});
                         }
