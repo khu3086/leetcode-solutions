6985adab100cf3055afe4e731b0e2fc9218838d9
Khushi <khushi.nigamwork@gmail.com>
2026-07-26 12:31:14 -0700

    Time: 0 ms (100.00%) | Memory: 93.4 MB (73.31%) - LeetSync

--- 2904-sorting-three-groups/sorting-three-groups.cpp
+++ 2904-sorting-three-groups/sorting-three-groups.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> dp(n, 1);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[j]<=nums[i]){
-                    dp[i]=max(dp[j]+1, dp[i]);
-                }
+        int dp1=0;
+        int dp2=0;
+        int dp3=0;
+        for(auto num: nums){
+            if(num==1){
+                dp2++;
+                dp2=min(dp2, dp1);
+                dp3++;
+                dp3=min(dp3, dp2);
+            }
+            if(num==2){
+                dp1++;
+                dp2=min(dp1, dp2);
+                dp3++;
+                dp3=min(dp3, dp2);
+            }
+            if(num==3){
+                dp1++;
+                dp2++;
+                dp2=min(dp1, dp2);
+                dp3=min(dp2, dp3);
             }
         }
-        int maxLen=INT_MIN;
-        for(auto m: dp) maxLen=max(maxLen, m);
-        return n-maxLen;
+        return min(dp1, min(dp2, dp3));
     }
 };
