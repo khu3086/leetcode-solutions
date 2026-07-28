a6624a6d5195140a4cac078fb6a89bac39712c6a
Khushi <khushi.nigamwork@gmail.com>
2026-07-28 09:59:32 -0700

    Time: 3 ms (31.75%) | Memory: 11.3 MB (17.93%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
-    void recurse(vector<vector<int>>& ans, int i, int n, vector<int> nums, vector<int> curr){
-        if(i>n) return;
-        if(i==n){
-            ans.push_back(curr);
-            return;
-        }
-        curr.push_back(nums[i]);
-        recurse(ans, i + 1, n, nums, curr);
-        curr.pop_back();
+    void recurse(vector<vector<int>>& ans, int i, int n,
+                 vector<int> nums, vector<int> curr) {
 
-        // Don't take nums[i]
-        int j = i + 1;
-        while(j < n && nums[j] == nums[i])
+        ans.push_back(curr);
+
+        int j = i;
+
+        while(j < n) {
+            curr.push_back(nums[j]);
+
+            recurse(ans, j + 1, n, nums, curr);
+
+            curr.pop_back();
+
+            while(j + 1 < n && nums[j] == nums[j + 1])
+                j++;
+
             j++;
-
-        recurse(ans, j, n, nums, curr);
+        }
     }
+
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
-        int n=nums.size();
-        int i=0;
+        int n = nums.size();
+        int i = 0;
         vector<int> curr;
+
         recurse(ans, i, nums.size(), nums, curr);
+
         return ans;
     }
 };
