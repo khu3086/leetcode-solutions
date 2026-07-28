5a00b332c0f684c67c25384a4885a6c75ef9a044
Khushi <khushi.nigamwork@gmail.com>
2026-07-28 14:03:17 -0700

    Time: 66 ms (66.16%) | Memory: 20.9 MB (70.49%) - LeetSync

--- 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.cpp
+++ 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     int maxFreq(string s, int maxLetters, int minSize, int maxSize) {
-        unordered_map<char, int> st;
-        int n=s.size();
-        int j=0;
-        int count=0;
+        unordered_map<char, int> mp;
+        unordered_map<string, int> strs;
         int maxOccur=0;
-        unordered_map<string, int> mp;
+        int j=0;
+        int n=s.length();
         for(int i=0;i<n;i++){
-            st[s[i]]++;
-
-            while (st.size() > maxLetters) {
-                st[s[j]]--;
-
-                if (st[s[j]] == 0)
-                    st.erase(s[j]);
-
+            mp[s[i]]++;
+            while(mp.size()>maxLetters){
+                mp[s[j]]--;
+                if(mp[s[j]]==0) mp.erase(s[j]);
                 j++;
             }
-            int currLen=i-j+1;
-            if (i - j + 1 >= minSize) {
+            int len=i-j+1;
+            if(len>=minSize){
                 string sub = s.substr(i - minSize + 1, minSize);
-
-                mp[sub]++;
-                maxOccur = max(maxOccur, mp[sub]);
+                strs[sub]++;
+                maxOccur=max(maxOccur, strs[sub]);
             }
         }
         return maxOccur;
