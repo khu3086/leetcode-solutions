c181a076ccd23cdee22b0560bb4142b7cd0621c6
Khushi <khushi.nigamwork@gmail.com>
2026-07-28 05:47:22 -0700

    Time: 40 ms (37.46%) | Memory: 61.5 MB (35.89%) - LeetSync

--- 630-course-schedule-iii/course-schedule-iii.cpp
+++ 630-course-schedule-iii/course-schedule-iii.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int scheduleCourse(vector<vector<int>>& courses) {
-        vector<pair<int, int>> myarr;
-        int n=courses.size();
-        for(int i=0;i<n;i++){
-            myarr.push_back({courses[i][1], courses[i][0]});
-        }
-        sort(myarr.begin(), myarr.end());
-        priority_queue<int> pq;
-        int time=0;
-        for(int i=0;i<n;i++){
-            time+=myarr[i].second;
-            pq.push(myarr[i].second); //push deadline
-            if(time>myarr[i].first) {time-=pq.top(); pq.pop();}
+        priority_queue<pair<int, int>> pq;
+        int currDeadline=0;
+        sort(courses.begin(), courses.end(),
+     [](vector<int>& a, vector<int>& b) {
+         return a[1] < b[1];
+     });
+        for(int i=0;i<courses.size();i++){
+            currDeadline+=courses[i][0];
+            pq.push({courses[i][0], courses[i][1]});
+            if(!pq.empty()&&currDeadline>courses[i][1]){
+                auto top=pq.top(); pq.pop();
+                currDeadline-=top.first;
+            }
         }
         return pq.size();
     }
